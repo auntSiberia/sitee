24a3fa7c07db922508ccd7fda38a74174cb2f5e7
auntSiberia <151033265+auntSiberia@users.noreply.github.com>
2024-04-29 19:48:42 +0500

    Add files via upload

---
+++ Project3/users/views.py
@@ -0,0 +1,70 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.contrib import auth, messages
+from django.urls import reverse
+from users.forms import UserLoginForm, UserRegistrationForm, UserChangeForm, ProfileForm
+from django.contrib.auth.decorators import login_required
+
+
+def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                messages.success(request, f"{username}, вы вошли в аккаунт")
+                redirect_page = request.POST.get("next", None)
+                if redirect_page and redirect_page != reverse("user:logout"):
+                    return HttpResponseRedirect(request.POST.get("next"))
+                return HttpResponseRedirect(reverse("main:index"))
+
+    else:
+        form = UserLoginForm()
+
+    context = {"title": "Авторизация", "form": form}
+    return render(request, "users/login.html", context)
+
+
+def registration(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            messages.success(request, f"{user.username}, вы зарегистрировались")
+            return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserRegistrationForm()
+    context = {"title": "Регистрация", "form": form}
+    return render(request, "users/registration.html", context)
+
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        form = ProfileForm(
+            data=request.POST, instance=request.user, files=request.FILES
+        )
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профиль обновлен")
+            return HttpResponseRedirect(reverse("user:profile"))
+    else:
+        form = ProfileForm(instance=request.user)
+    context = {"title": "Кабинет", "form": form}
+    return render(request, "users/profile.html", context)
+
+
+def users_cart(request):
+    return render(request, "users/users_cart.html")
+
+
+def logout(request):
+    messages.success(request, f"{request.user.username}, вы вышли из аккаунта")
+    auth.logout(request)
+    return redirect(reverse("main:index"))
